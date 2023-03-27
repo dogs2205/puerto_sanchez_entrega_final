0e13203c541a3e84d322c43bd02c3f396992bf86
NICOLAS_PUERTO <puertonicolas98@gmail.com>
2023-03-26 20:50:14 -0500

    ajuste con clase 24

--- project/urls.py
+++ project/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="index"),
     path('animal/list', AnimalList.as_view(), name="animal-list"),
-    path('platillo/list', AnimalMineList.as_view(), name="animal-mine"),
+    path('animal/list/', AnimalMineList.as_view(), name="animal-mine"),
     path('animal/<pk>/update', AnimalUpdate.as_view(), name="animal-update"),
     path('animal/<pk>/delete', AnimalDelete.as_view(), name="animal-delete"),
     path('animal/create', AnimalCreate.as_view(), name="animal-create"),
